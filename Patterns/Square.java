ccc471d43af0bd3f27ef4bb6d4c18efaa54bea13
vaibhav sathe <vaibhavsathe160@gmail.com>
2026-06-20 15:18:15 +0530

    Replace invertedPyramid with fullPyramid in main method and add invertedNumberPyramid and fullPyramid methods

--- Patterns/Square.java
+++ Patterns/Square.java
@@ -51,11 +51,38 @@ public class Square{
     }
   }
 
+  public static void invertedNumberPyramid(int n){
+    for(int i = 0; i < n; i++){
+      int count = 1;
+      for(int j = 0; j < n-i;j++){
+        System.out.print(count + " ");
+        count++;
+      }
+      System.out.println();
+    }
+  }
+
+  public static void fullPyramid(int n){
+    for(int i = 0; i < n; i++){
+      // space
+      for(int j = 0; j < n-i; j++){
+        System.out.print(" ");
+      }
+
+      // star "*"
+      for(int j = 0; j < 2*i+1; j++){
+        System.out.print("*" );
+      }
+      System.out.println();
+    }
+  }
+
+
   public static void main(String[] args) {
     int t = 3;
     int n = 3;
     for(int i = 0; i < t; i++){
-      invertedPyramid(n);
+      fullPyramid(n);
       n++;
     }
     
