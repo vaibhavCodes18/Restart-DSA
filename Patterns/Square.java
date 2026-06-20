eb8f8e738789308c41eef5d3debe63910a6132b6
vaibhav sathe <vaibhavsathe160@gmail.com>
2026-06-20 17:42:16 +0530

    Add full inverted pyramid, binary half pyramid, and additional patterns

--- Patterns/Square.java
+++ Patterns/Square.java
@@ -77,15 +77,73 @@ public class Square{
     }
   }
 
+  public static void fullInvertedPyramid(int n){
+    for(int i = n-1; i >= 0; i--){
+      // space
+      for(int j = 0; j < n-i; j++){
+        System.out.print(" ");
+      }
+
+      // star "*"
+      for(int j = 0; j < 2*i+1; j++){
+        System.out.print("*" );
+      }
+      System.out.println();
+    }
+  }
+
+  public static void binaryHalfPyramid(int n){
+    int start;
+    for(int i = 0; i < n; i++){
+      if(i%2==0) start = 1;
+      else start = 0;
+      for(int j = 0; j <= i; j++){
+        System.out.print(start);
+        start = 1 - start;
+      }
+      System.out.println();
+    }
+  }
+
+  public static void pattern12(int n){
+    int space = 2*n-1;
+    for(int i = 1; i <= n; i++){
+      // numbers
+      for(int j = 1; j <= i; j++){
+        System.out.print(j);
+      }
+      // space
+      for(int j = 1; j <= space; j++){
+        System.out.print(" ");
+      }
+      // numbers
+      for(int j = i; j >= 1; j--){
+        System.out.print(j);
+      }
+      System.out.println();
+      space -= 2;
+    }
+  }
+
+  public static void pattern13(int n){
+    int count = 1;
+    for(int i = 1; i <= n; i++){
+      for(int j = i; j >= 1; j--){
+        System.out.print(count);
+        count++;
+      }
+      System.out.println();
+    }
+  } 
 
   public static void main(String[] args) {
-    int t = 3;
-    int n = 3;
-    for(int i = 0; i < t; i++){
-      fullPyramid(n);
-      n++;
-    }
-    
+    // int t = 3;
+    // int n = 3;
+    // for(int i = 0; i < t; i++){
+    //   pattern12(n);
+    //   n++;
+    // }
+    pattern13(4);
   }
 
 }
