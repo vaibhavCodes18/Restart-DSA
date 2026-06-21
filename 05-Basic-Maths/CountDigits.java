911e413aac9604e5212a93dc3e914b9eb2e52a0f
vaibhav sathe <vaibhavsathe160@gmail.com>
2026-06-21 21:59:39 +0530

    Refactor CountDigits class: move reverseNumber and palindrome methods, update main method to test palindrome

--- 05-Basic-Maths/CountDigits.java
+++ 05-Basic-Maths/CountDigits.java
@@ -11,14 +11,48 @@ public class CountDigits {
 
     return count;
   }
-  
+
+  public static int reverseNumber(int n) {
+    if(n==0) return 0;
+      int rev = 0;
+      while(n > 0){
+        int lastDigit = n % 10;
+        rev = rev*10 + lastDigit;
+        n /= 10;
+      }
+      return rev;
+  }
+
+  public static boolean palindrome(int n) {
+      int dup = n;
+		  int rev = 0;
+		  while(n>0){
+        int ld = n % 10;
+        n /= 10;
+        rev = rev*10 + ld;
+		  }
+		return dup == rev;
+  }
+
+  public static boolean amstrong(int n) {
+    int duplicate = n;
+    int sum = 0;
+    while(n>0){
+      int ld = n % 10;
+      sum += (ld*ld*ld);
+      n/=10;
+    }
+    return sum == duplicate;
+  }
+
+
   // brute force approach
   public static int countDigitsOptimize(int num) {
     return (int)(Math.log10(num)+1);
   }
 
   public static void main(String[] args) {
-    int result = countDigitsOptimize(125);
+    boolean result = amstrong(1634);
     System.out.println(result);
   }
 }
