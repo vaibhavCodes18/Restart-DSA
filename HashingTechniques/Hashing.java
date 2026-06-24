08e913451ff32c5accaedadcfc5239da56f8363b
vaibhav sathe <vaibhavsathe160@gmail.com>
2026-06-24 18:17:00 +0530

    Add Hashing class with countFrequency method to calculate and display element frequency in an array

---
+++ HashingTechniques/Hashing.java
@@ -0,0 +1,27 @@
+// package HashingTechniques;
+import java.util.*;
+
+public class Hashing {
+
+  public static void countFrequency(int[] arr) {
+    Map<Integer, Integer> map = new HashMap<>();
+
+    for(int i:arr){
+      // if(map.get(i) != null){
+
+      //   map.put(i, map.get(i) + 1);
+      // }else{
+      //   map.put(i, 0 + 1);
+      // }
+      
+      // or else we have another options that is getOrDefault(K,V) with set default value at the time of get element if key not exist
+      map.put(i, map.getOrDefault(i, 0) + 1);
+    }
+    System.out.println(map);
+  }
+
+  public static void main(String[] args) {
+    int[] arr = {1, 2, 3,8, 1, 5, 6,5,6};
+    countFrequency(arr);
+  }
+}
