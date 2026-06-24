4536322509a41d4d28d9d62a408c613679b2aff5
vaibhav sathe <vaibhavsathe160@gmail.com>
2026-06-24 23:30:01 +0530

    Implement bubbleSort method and update main method to use bubbleSort for array sorting

--- Sorting/Sorting.java
+++ Sorting/Sorting.java
@@ -24,9 +24,25 @@ public class Sorting {
     }
   }
 
+  public static void bubbleSort(int[] arr){
+    int n = arr.length;
+    for(int i = n-1; i >= 1; i--){
+      int didSwap = 0;
+      for(int j = 0; j <= i-1; j++){
+        if(arr[j] > arr[j+1]){
+          int temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
+          didSwap = 1;
+        }
+      }
+      if(didSwap == 0) break;
+    }
+  }
+
   public static void main(String[] args) {
     int[] arr = {1, 2, 3, 8, 1, 5, 6, 5, 6};
-    selectionSort(arr);
+    bubbleSort(arr);
     printArray(arr);
   }
   
