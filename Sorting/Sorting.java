5ae906073949a49e609106f0d505db37f9220909
vaibhav sathe <vaibhavsathe160@gmail.com>
2026-06-26 00:57:50 +0530

    Implement merge sort algorithm and update main method to use mergeSort for array sorting

--- Sorting/Sorting.java
+++ Sorting/Sorting.java
@@ -1,4 +1,5 @@
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Sorting {
 
@@ -53,9 +54,43 @@ public class Sorting {
     }
   }
 
+  public static void mergeSort(int[] arr, int low, int high) {
+    if(low==high) return;
+    int mid = (low+high)/2;
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid+1, high);
+    merge(arr, low, mid, high);
+  }
+
+  public static void merge(int[] arr, int low, int mid, int high) {
+    List<Integer> temp = new ArrayList<>();
+    int left = low, right = mid+1;
+    while(left <= mid && right <= high){
+      if(arr[left] <= arr[right]){
+        temp.add(arr[left]);
+        left++;
+      }
+      else{
+        temp.add(arr[right]);
+        right++;
+      }
+    }
+    while(left <= mid){
+      temp.add(arr[left]);
+      left++;
+    }
+    while(right <= high){
+      temp.add(arr[right]);
+      right++;
+    }
+    for(int i = low; i <= high; i++){
+      arr[i] = temp.get(i-low);
+    }
+  }
+
   public static void main(String[] args) {
     int[] arr = {1, 2, 3, 8, 1, 5, 6, 5, 6};
-    insertionSort(arr);
+    mergeSort(arr, 0, arr.length-1);
     printArray(arr);
   }
   
