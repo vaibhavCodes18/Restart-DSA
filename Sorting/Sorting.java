c6e4b47f20955cc7afd38bf1290deb4f121a2147
vaibhav sathe <vaibhavsathe160@gmail.com>
2026-06-26 22:28:13 +0530

    Implement quicksort algorithm and update main method to use qs for array sorting

--- Sorting/Sorting.java
+++ Sorting/Sorting.java
@@ -88,9 +88,40 @@ public class Sorting {
     }
   }
 
+  public static int partition(int[] arr, int low, int high){
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+    while(i < j){
+      while(arr[i] <= pivot && i <= high-1){
+        i++;
+      }
+      while(arr[j] >= pivot && j >= low+1){
+        j--;
+      }
+      if(i<j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+    int temp = arr[low];
+    arr[low] = arr[j];
+    arr[j] = temp;
+    return j;
+  }
+
+  public static void qs(int[] arr, int low, int high) {
+    if(low < high){
+      int pIndex = partition(arr, low, high);
+      qs(arr, low, pIndex - 1);
+      qs(arr, pIndex + 1, high);
+    }
+  }
+
   public static void main(String[] args) {
     int[] arr = {1, 2, 3, 8, 1, 5, 6, 5, 6};
-    mergeSort(arr, 0, arr.length-1);
+    qs(arr, 0, arr.length-1);
     printArray(arr);
   }
   
