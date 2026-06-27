6fac1e2ba6841f13e87cffbe984a35694d3be6d3
vaibhav sathe <vaibhavsathe160@gmail.com>
2026-06-27 23:30:36 +0530

    Refactor secondLargest method to improve logic for finding the second largest element

--- Arrays-Problem/ArraysProblem.java
+++ Arrays-Problem/ArraysProblem.java
@@ -30,16 +30,28 @@ public class ArraysProblem {
     // }
 
     // Better
+    // int largest = arr[0];
+    // int slargest = -1;
+    // for(int i = 0; i < n; i++){
+    //   if(arr[i] >= largest){
+    //     largest = arr[i];
+    //   }
+    // }
+
+    // for(int i = 0; i < n; i++){
+    //   if(arr[i] > slargest && arr[i] != largest){
+    //     slargest = arr[i];
+    //   }
+    // }
+
+    // optimal
     int largest = arr[0];
     int slargest = -1;
     for(int i = 0; i < n; i++){
-      if(arr[i] >= largest){
+      if(arr[i] > largest){
+        slargest = largest;
         largest = arr[i];
-      }
-    }
-
-    for(int i = 0; i < n; i++){
-      if(arr[i] > slargest && arr[i] != largest){
+      }else if(arr[i] < largest && arr[i] > slargest){
         slargest = arr[i];
       }
     }
