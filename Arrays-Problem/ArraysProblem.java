ec90f179f4fe7e3b7a9eb745c30eae53695802a6
vaibhav sathe <vaibhavsathe160@gmail.com>
2026-06-27 17:26:19 +0530

    Add ArraysProblem class with largestElement method to find the largest number in an array

---
+++ Arrays-Problem/ArraysProblem.java
@@ -0,0 +1,32 @@
+
+public class ArraysProblem {
+
+  public static int largestElement(int[] arr) {
+    int n = arr.length;
+
+    // Arrays.sort(arr);
+    // return arr[n-1];
+
+    int max = arr[0];
+    for(int i = 0; i < n; i++){
+      if(arr[i] >= max){
+        max = arr[i];
+      }
+    }
+    return max;
+  }
+
+  public static void main(String[] args) {
+    
+    int[] arr1 = {1, 2, 3, 8, 8, 1, 5, 6, 5, 6};
+    int[] arr2 = {13, 2, 23, 8, 84};
+    int[] arr3 = {-1, -23, -3, -82, 0, 1};
+    int[][] arrays = {arr1,arr2,arr3};
+    int t = arrays.length;
+
+    for(int i = 0; i < t; i++){
+      int res = largestElement(arrays[i]);
+      System.out.println(res);
+    }
+  }
+}
