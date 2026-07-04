4c264acc48342acb3b81858805a53da10e104ad3
vaibhav sathe <vaibhavsathe160@gmail.com>
2026-07-04 16:41:42 +0530

    Add Duplicate_Number_287 class with brute force method to find duplicate elements in an array

---
+++ Arrays-Problem/Duplicate_Number_287.java
@@ -0,0 +1,26 @@
+public class Duplicate_Number_287 {
+
+  public static int duplicateElementBruteForce(int[] arr, int n) {
+    for(int i = 0; i < n; i++){
+      int num = arr[i];
+      for(int j = 0; j < n; j++){
+        if(i != j && num == arr[j]) return num;
+      }
+    }
+    return 0;
+  }
+
+  public static void main(String[] args) {
+    int[] arr1 = {1,3,4,2,2};
+        int[] arr2 = {3,2,3};
+        int[] arr3 = {3,1,3,4,2};
+        int[][] arrays = {arr1, arr2, arr3};
+        int t = arrays.length;
+
+        for (int i = 0; i < t; i++) {
+            int arr = duplicateElementBruteForce(arrays[i], arrays[i].length);
+            // print
+            System.out.println(arr);
+        }
+  }
+}
